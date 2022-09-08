4a863599ec7602dc2628ac71748bfbde0c445fbb
Rafael <rafael-srv@hotmail.com>
2022-09-08 11:54:12 -0300

    DBImplement

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,10 +1,29 @@
+import {useEffect, useState} from 'react'
 import Input from '../form/Input'
 import styles from './ProjectForm.module.css'
 import Select from '../form/Select'
 import SubmitButton from '../form/submitButton'
 
 function ProjectForm({btnText}){
+    
+    const [categories, setCategories] = useState([])
+
+    useEffect(()=>{
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((resp) => resp.json())
+            .then((data)=> {
+                setCategories(data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
     return(
+
     <form className={styles.form}>
         <Input 
         type="text" 
@@ -19,8 +38,14 @@ function ProjectForm({btnText}){
         name="budget" 
         placeholder="Insira o orçamento total"
         />
-        <Select name="category_id" text="Selecione a categoria"/>
+        
+        <Select 
+        name="category_id" 
+        text="Selecione a categoria " 
+        options={categories}/>
+
         <SubmitButton text={btnText}/>
+
     </form>
     )
 }

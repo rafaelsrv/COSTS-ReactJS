945c854c36bcb7119de5b7320f8442ffb61e8783
Rafael <rafael-srv@hotmail.com>
2022-09-10 00:28:59 -0300

    ImplementaçãoVisualEditDelete

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,16 +1,37 @@
 import { useLocation } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import Message from "../layouts/Message"
 import styles from './Projects.module.css'
 import Container from '../layouts/Container'
 import LinkButton from '../layouts/LinkButton'
+import ProjectCard from '../project/ProjectCard'
 
 function Projects(){
+    const[projects, setProjects] = useState([])
 
     const location = useLocation()
     let message = ''
     if (location.state) {
       message = location.state.message
     }
+
+    useEffect(()=>{
+
+      fetch('http://localhost:5000/projects',{
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+
+      })
+      .then((resp) => resp.json())
+      .then((data) =>{
+        console.log(data)
+        setProjects(data)
+
+      })
+      .catch((err) => console.log(err))
+    },[])
             
     return (
     <div className={styles.project_container}>
@@ -21,9 +42,18 @@ function Projects(){
         
         {message && <Message type="success" msg={message}/>}
         <Container customClass="start">
-          <p>Projetos...</p>
-        </Container>
+          {projects.length > 0 && 
+            projects.map((project) =>                       //Map feito para mostrar os projetos existentes.
+            <ProjectCard 
+            name={project.name}
+            id={project.id}
+            budget={project.budget}
+            category={project.category.name}
+            key={project.id}
 
+            />
+          )}
+        </Container>
     </div>
     )
 }

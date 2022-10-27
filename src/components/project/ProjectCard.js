61c539f74014499c2ddeedb2d94a6c915d6116d1
Rafael <rafael-srv@hotmail.com>
2022-10-26 21:24:05 -0300

    Implementando modificações

--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -15,7 +15,7 @@ function ProjectCard({id, name, budget, category, handleRemove}){
         <div className={styles.project_card}>
             <h4>{name}</h4>
             <p>
-                <span>Orçamento</span> R${budget}
+                <span>Orçamento:</span> R${budget}
             </p>
             <p className={styles.category_text}>
                 <span className={`${styles[category.toLowerCase()]}`}></span> {category}

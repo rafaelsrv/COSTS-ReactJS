5c4524024ec6c0acc6240c5570247071daa2b510
Rafael <rafael-srv@hotmail.com>
2022-10-27 19:50:24 -0300

    comentando código

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -2,6 +2,7 @@ import styles from './Select.module.css'
 function Select({text, name, options, handleOnChange, value}){
     //Selecionar opções, recebendo props.
     //Recebe text, name, options
+    //Recebe função handleOnChange
     return(
     <div className={styles.form_control}>
         <label htmlFor="{name}">{text}</label>
